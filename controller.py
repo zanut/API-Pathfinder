c1536fffccf863a56eb8df51d368c77a0b2610a8
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2024-05-12 22:43:07 +0700

    add get average weather from timestamp

--- controller.py
+++ controller.py
@@ -102,6 +102,27 @@ def get_average_weather_by_date_hour(date, hour):
         return result
 
 
+def get_average_weather_between_timestamp(begin_timestamp, end_timestamp):
+    with pool.connection() as conn, conn.cursor() as cs:
+        cs.execute("""
+            SELECT 
+                ROUND(AVG(temp), 4) AS avg_temp, 
+                ROUND(AVG(hum), 4) AS avg_hum, 
+                ROUND(AVG(pres), 4) AS avg_pres, 
+                ROUND(AVG(dp), 4) AS avg_dp, 
+                ROUND(AVG(uvi), 4) AS avg_uvi, 
+                ROUND(AVG(cloud), 4) AS avg_cloud, 
+                ROUND(AVG(vis), 4) AS avg_vis,
+                wmain,
+                ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM API_weather WHERE Timestamp >= %s AND Timestamp <= %s), 4) AS occurrence_percentage
+            FROM API_weather
+            WHERE Timestamp >= %s AND Timestamp <= %s
+            GROUP BY wmain
+        """, (begin_timestamp, end_timestamp, begin_timestamp, end_timestamp))
+        result = [models.AverageWeather(*row) for row in cs.fetchall()]
+        return result
+
+
 def get_traffic_all():
     with pool.connection() as conn, conn.cursor() as cs:
         cs.execute("""
