dfbdcd514adda7f351aac077592331d72ecec20d
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2024-05-12 14:19:14 +0700

    add get average weathe bby date

--- controller.py
+++ controller.py
@@ -60,3 +60,34 @@ def get_weather_by_timestamp(begin, end):
         return result
 
 
+def get_average_weather_by_date(date):
+    with pool.connection() as conn, conn.cursor() as cs:
+        cs.execute("""
+            SELECT 
+                ROUND(AVG(temp), 4) AS avg_temp, 
+                ROUND(AVG(hum), 4) AS avg_hum, 
+                ROUND(AVG(pres), 4) AS avg_pres, 
+                ROUND(AVG(dp), 4) AS avg_dp, 
+                ROUND(AVG(uvi), 4) AS avg_uvi, 
+                ROUND(AVG(cloud), 4) AS avg_cloud, 
+                ROUND(AVG(vis), 4) AS avg_vis,
+                wmain,
+                ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM API_weather WHERE DATE(Timestamp) = %s), 4) AS occurrence_percentage
+            FROM API_weather
+            WHERE DATE(Timestamp) = %s
+            GROUP BY wmain
+        """, (date, date))
+        result = [{
+            'wmain': row[7],
+            'occurrence_percentage': row[8],
+            'avg_temp': row[0],
+            'avg_hum': row[1],
+            'avg_pres': row[2],
+            'avg_dp': row[3],
+            'avg_uvi': row[4],
+            'avg_cloud': row[5],
+            'avg_vis': row[6]
+        } for row in cs.fetchall()]
+        return result
+
+
